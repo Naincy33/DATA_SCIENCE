780f1175bb9382dd21dce077992325315c8e5a5e
Naincy33 <iamnaincy33@gmail.com>
2026-05-20 20:39:00 +0530

    sigmoid function

---
+++ ML/sigmoid_function.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-10, 10, 100)
+
+y = 1 / (1 + np.exp(-x))
+
+plt.plot(x, y)
+
+plt.title("Sigmoid Function")
+plt.xlabel("x")
+plt.ylabel("sigmoid(x)")
+
+plt.grid()
+plt.show()
